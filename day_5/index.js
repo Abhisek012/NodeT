7027862fa371b244f5a1d402183da142e1009ba5
Abhisek Prusty <abhisekprusty018@gmial.com>
2026-01-16 21:27:16 +0530

    add index.js for crypto modules

--- day_5/index.js
+++ day_5/index.js
@@ -1,1 +1,25 @@
-console.log("hi");
+const crypto = require("crypto");
+
+
+// randomBytes
+// const randomvalues  = crypto.randomBytes(10);
+
+// console.log(randomvalues.toString("ascii")); //inside () we can mention base64 or ascii 
+
+
+
+// hashvalue
+const hashvalue = crypto.createHash("sha256").update("Abhisek").digest("hex")
+
+const inputvalue = "Abhisek"
+const matchvalue = crypto.createHash("sha256").update(inputvalue).digest("hex")
+
+if(hashvalue == matchvalue){
+    console.log("You can login!")
+
+}else{
+    console.log("Something went wrong!")
+}
+
+// encryption and decryption in node.js
+// have to learn myself 
